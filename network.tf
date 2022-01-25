142ab22488d2952ffe68ed71aa1f22d375376ae8
avoidik <avoidik@gmail.com>
2022-01-25 11:52:43 +0000

    add vpc module, fix typo

--- network.tf
+++ network.tf
@@ -4,7 +4,7 @@ module "network" {
   # insert required variables here
   network_name = "gaurav-network"
   project_id = var.project
-  subnets = subnets = [
+  subnets = [
     {
       subnet_name   = "gaurav-subnet"
       subnet_ip     = "10.100.10.0/24"
